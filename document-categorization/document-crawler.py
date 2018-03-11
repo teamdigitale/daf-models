46ee31b64c7b385a17bbd7301459cc02b16d12c3
Fabio Fumarola <fabiofumarola@gmail.com>
2018-03-11 20:23:06 +0100

    closes #1

--- document-categorization/document-crawler.py
+++ document-categorization/document-crawler.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[38]:
+# In[2]:
 
 
 import urllib3
@@ -12,11 +12,15 @@ import os
 import logging
 
 
-# In[39]:
+# # Crawl official documents from Tuscany Region
+
+# Setup the loggers
+
+# In[3]:
 
 
-logger = logging.getLogger('crawler_application')
-logger.setLevel(logging.DEBUG)
+log = logging.getLogger('crawler_application')
+log.setLevel(logging.DEBUG)
 fh = logging.FileHandler('crawler.log')
 ch = logging.StreamHandler()
 
@@ -25,89 +29,140 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 fh.setFormatter(formatter)
 ch.setFormatter(formatter)
 # add the handlers to the logger
-logger.addHandler(fh)
-logger.addHandler(ch)
+log.addHandler(fh)
+log.addHandler(ch)
 
 
-# In[40]:
+# Define output folder, output filename pattern
+
+# In[4]:
 
 
 out_folder = 'regione-toscana'
-file_name = 'regione-toscana-result-{:d}-{:d}.json'
+
+act_types = {
+    'atti_dirigenti': 'DMON',
+    'atti_presidente': 'DPG',
+    'atti_giunta': 'DAD'
+}
+
+file_name = 'regione-toscana-result-type_{}-year_{:d}-from_{:d}-to_{:d}.json'
+query = "http://www.regione.toscana.it/bancadati/search?site=atti&client=fend_json&output=xml_no_dtd&getfields=*&ulang=it&ie=UTF-8&proxystylesheet=fend_json&start={:d}&num={:d}&filter=0&rc=1&q=inmeta%3AID_TIPO_PRATICA%3{}+AND+inmeta%3ADATA_ATTO%3A{}-01-01..{}-12-31+AND+inmeta%3ANUMERO%3A{}..{}+AND+inmeta%3AALLEGATO_DESCRIZIONE%3Dvoid&sort=meta%3ACODICE_PRATICA%3AA"
 
 
-# In[41]:
+# In[5]:
 
 
 if not os.path.exists(out_folder):
     os.makedirs(out_folder)
 
 
-# In[42]:
+# In[6]:
 
 
 http_pool = urllib3.PoolManager()
 
 
-# In[43]:
+# In[7]:
 
 
-url_base = "http://www.regione.toscana.it/bancadati/search?site=atti&client=fend_json&output=xml_no_dtd&getfields=*&ulang=it&ie=UTF-8&proxystylesheet=fend_json&start={:d}&num={:d}&filter=0&rc=1&q=inmeta%3AALLEGATO_DESCRIZIONE%3Dvoid&sort=meta%3ACODICE_PRATICA%3AD"
+year = 2017
+start = 0
+num = 200
+from_number = 0
+to_number = 200
 
 
-# In[46]:
+# In[8]:
 
 
-def download_data(start, num):
-    logger.info("start {}, num {}".format(start,num))
-    sleep_time = random.randint(10,30)
-    logger.debug('sleeping {} seconds'.format(sleep_time))
+example = query.format(start, num, act_types['atti_giunta'], year, year, from_number, to_number)
+
+
+# In[9]:
+
+
+example
+
+
+# In[10]:
+
+
+def download_data(start, num, act_name, year, from_number, to_number):
+
+    log.info("start {}, num {}, act_name {}, year {}, from_number {}, to_number {}".format(
+        start,num, act_name, year, from_number, to_number))
+    sleep_time = random.randint(2,5)
+    log.debug('sleeping {} seconds'.format(sleep_time))
     time.sleep(random.randint(5,20))
-    
-    output_name = out_folder + "/" + file_name.format(start, start + num)
-    url = url_base.format(start, num)
-    logger.info('requesting url {}'.format(url))
+
+    url = query.format(start, num + 1, act_types[act_name], year, year, from_number, to_number)
+    log.info('requesting url {}'.format(url))
     res = http_pool.request('GET', url, retries=1)
-    logger.info('got status {} for url {}'.format(res.status, url))
-    
+    log.info('got status {} for url {}'.format(res.status, url))
+
     num_res = 0
-    
+
     if res.status is 200:
         json_data = json.loads(res.data.decode('utf-8'))
         if 'RES' in json_data['GSP']:
             num_res = len(json_data['GSP']['RES']['R'])
-            with open(output_name, 'w') as f:
+
+            out_file_name = out_folder + "/" + file_name.format(act_name, year, from_number, to_number)
+
+            with open(out_file_name, 'w') as f:
                 f.write(res.data.decode('utf-8'))
-                logger.info('saved result in {}'.format(output_name))
+                log.info('saved result in {}'.format(out_file_name))
         else:
-            logger.error('no results skipped {} \n {}'.format(url, json_data))
+            log.error('no results skipped {} \n {}'.format(url, json_data))
     else:
-        logger.error('status {} skipped {}'.format(res.status, url))
-    
-    status = res.status
+        log.error('status {} skipped {}'.format(res.status, url))
+
     res.close()
     return num_res
-    
 
 
-# In[47]:
+
+# In[11]:
 
 
-start = 1000
-num = 100
-
-num_records = 1
-counter = 0
-
-while num_records > 0:
-    num_records = download_data(start, num)
-    counter += num_records
-    start += num
-    logger.info('extracted {} records'.format(counter))
+# download_data(start, num, 'atti_giunta', year, from_number, to_number)
 
 
-# In[24]:
+# In[12]:
+
+
+years = list(range(2002,2019))
+print(years)
+
+
+# In[13]:
+
+
+for act_name in act_types.keys():
+    counter = 0
+    for year in years:
+        log.info('start crawling document of type {} for the year {}'.format(act_name, year))
+        num_records = 1
+        start = 0
+        num = 200
+        from_number = 0
+        to_number = 200
+        while num_records > 0:
+            num_records = download_data(start, num, act_name, year, from_number, to_number)
+            counter += num_records
+            from_number = to_number
+            to_number += num
+        log.info('end crawling document of type {} for the year {} total number {}'.format(act_name, year, counter))
+
+
+# In[ ]:
 
 
 #json_data['GSP']['RES']['R'][1]
 
+
+# In[ ]:
+
+
+http_pool.clear()
